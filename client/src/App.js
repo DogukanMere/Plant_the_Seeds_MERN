abb17451e51e898eb2d06e8a49ee05343848587d
Dogukan <meredogukan@gmail.com>
2022-09-09 20:02:12 +0300

    frontend Home screen and Navigation setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to Plant the Seeds</h1>
+      <Header />
+      <main className='py-4'>
+        <Container>
+          <h1 className='fs-3'>WELCOME TO PLANT THE SEEDS</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };
